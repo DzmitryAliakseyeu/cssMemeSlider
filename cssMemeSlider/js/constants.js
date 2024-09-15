68f87b7df9e6adc8d8a30e86a344f3b526cc2805
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-09-15 12:06:27 +0200

    fix: fix slider style (Sun, Sep 15, 2024 12:06:53 PM)

--- cssMemeSlider/js/constants.js
+++ cssMemeSlider/js/constants.js
@@ -2,4 +2,6 @@ export const allSliderPoints = document.querySelectorAll('.controller__point');
 export const allSliderImages = document.querySelectorAll('.slider-image'); 
 export const sliderContainer = document.querySelector('.slider__container-images');
 export const textAll = document.querySelectorAll('.text');
+export const imagesBlock = document.querySelector('.images');
+export const textBlock = document.querySelector('.text__block');
 
--- cssMemeSlider/js/index.js
+++ cssMemeSlider/js/index.js
@@ -1,60 +1,36 @@
-import { allSliderPoints, sliderContainer, textAll } from "./constants.js";
+import { allSliderPoints, imagesBlock, textAll, textBlock } from "./constants.js";
 import { arrSlidesInfo } from "./objects.js";
 
 let lastPositionSlide = 0;
-const allSliderImages = document.querySelector('.slider__container-images').childNodes;
-
-const imagesBlock = document.querySelector('.images')
-
-console.log(textAll);
- textAll.forEach((el, i)=> el.innerText = arrSlidesInfo[i].text)
-
-
+textAll.forEach((el, i)=> el.innerText = arrSlidesInfo[i].text);
 
 allSliderPoints[0].children[0].classList.add('selected__point')
 allSliderPoints.forEach((el) => el.addEventListener('click', (e) => {
     if(document.querySelector('.selected__point')){
-        console.log('includes selected class');
         lastPositionSlide = document.querySelector('.selected__point').parentElement.id;
-        console.log(lastPositionSlide)
         document.querySelector('.selected__point').classList.remove('selected__point')
     }
-    console.log(el);
+
     const clickedEl = e.target;
     let id = clickedEl.id;
-
 
     if(clickedEl.className == 'point__circle'){
         clickedEl.classList.add('selected__point');
       
     } else {
         clickedEl.childNodes[0].classList.add('selected__point');
-        console.log(clickedEl.childNodes[0]);
     }
-   
 
     const parentElement = clickedEl.parentElement;
     if(!id) {
         id = parentElement.id;
     }
 
-    console.log(sliderContainer.children.length)
     if(id == 1 ){
         imagesBlock.style.transform = `translateX(${(id - 1) * 100}%)`;
-        // allSliderImages.forEach((el) => el.style = `margin-left: ${(id - 1) * 100}%`);
-        // allSliderImages.forEach((el) => el.style.transform = `translateX(${(id - 1) * 100}%)`);
-        textAll.forEach((el) => el.style = `left: ${(id - 1) * 100}%`);
-        console.log(`id === 0`)
-    } else if (id == sliderContainer.children.length){
-        imagesBlock.style.transform = `translateX(-${(id - 1) * 100 + 2}%)`;
-        // allSliderImages.forEach((el) => el.style.transform = `translateX(-${(id - 1) * 100 + 2}%)`);
-        // allSliderImages.forEach((el) => el.style = `margin-left: -${(id - 1) * 100 + 2}%`);
-        textAll.forEach((el) => el.style = `left: -${(id - 1) * 100 + 2}%`);
-    }  else {
+        textBlock.style.transform = `translateX(${(id - 1) * 100}%)`;
+    } else {
         imagesBlock.style.transform = `translateX(-${(id - 1) * 100}%)`;
-        // allSliderImages.forEach((el) => el.style.transform = `translateX(-${(id - 1) * 100}%)`);
-        // allSliderImages.forEach((el) => el.style = `margin-left: -${(id - 1) * 100}%`);
-        // allSliderImages.forEach((el) => el.style = `margin-left: -${(id - 1) * 100 + 1}%`);
-        textAll.forEach((el) => el.style = `left: -${(id - 1) * 100 + 1}%`);
+        textBlock.style.transform = `translateX(${-(id - 1) * 100}%)`;
     }
 }))
